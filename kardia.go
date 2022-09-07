36932a1ea6e3920009de23f4764f3d1e88abd69f
cpustejovsky <charles.pustejovsky@gmail.com>
2022-09-07 18:44:58 -0400

    feat: Add sendResult back to Beat method; add documentation

--- kardia.go
+++ kardia.go
@@ -1,27 +1,31 @@
 package kardia
 
 import (
-	"fmt"
 	"time"
 )
 
-type Heart interface {
-	Beat()
+// Result contains results that may be useful to have from a heartbeat pattern
+// TODO: Determine if Result struct is needed; if so, what else should be added other than current time?
+type Result struct {
+	Time time.Time
 }
+
+type resultsChan chan Result
 
 type heart struct {
 	done      <-chan any
 	Heartbeat chan any
 	pulse     <-chan time.Time
-	Results   chan time.Time
+	Results   resultsChan
 	workGen   <-chan time.Time
 }
 
+// New is a constructor function for heart; takes a done channel and pulse interval
 func New(done <-chan any, pulseInterval time.Duration) *heart {
 	pulse := time.Tick(pulseInterval)
 	workGen := time.Tick(2 * pulseInterval)
 	heartbeat := make(chan any)
-	results := make(chan time.Time)
+	results := make(resultsChan)
 	return &heart{
 		done:      done,
 		Heartbeat: heartbeat,
@@ -31,13 +35,16 @@ func New(done <-chan any, pulseInterval time.Duration) *heart {
 	}
 }
 
+// Beat is a method that sends Results and pulses until it reads from the done channel
 func (h *heart) Beat() {
 	defer close(h.Heartbeat)
+	defer close(h.Results)
 	for {
 		select {
 		case <-h.done:
-			fmt.Println("DONE!")
 			return
+		case r := <-h.workGen:
+			h.sendResult(r)
 		case <-h.pulse:
 			h.sendPulse()
 		}
@@ -58,7 +65,7 @@ func (h *heart) sendResult(r time.Time) {
 			return
 		case <-h.pulse:
 			h.sendPulse()
-		case h.Results <- r:
+		case h.Results <- Result{Time: r}:
 			return
 		}
 	}
